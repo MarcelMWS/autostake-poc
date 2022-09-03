9ff9d432eac9c541300fa1fe1c3d3bf18cdd3de8
Marcel Pohland <m.pohland@mwaysolutions.com>
2022-09-03 14:03:31 +0200

    description

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check dependencies.
-if [ ! -f /usr/local/bin/docker ]; then 
+if [ ! -f /usr/local/bin/docker ]; then
     echo "docker is not installed \"/usr/local/bin/\", please install and set your path ..."
     exit 1
 fi
@@ -11,22 +11,32 @@ docker compose up -d
 
 
 # Check gaiad dependencies.
-if [ ! -f ~/go/bin/gaiad ]; then 
-    echo "Test it with \"./bin/gaiad-v7.0.3-darwin-amd64 q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
+if [ ! -f ~/go/bin/gaiad ]; then
+    echo "Test it and type in your cli in this project dir:"
     sleep 1
-    echo "Test it with \"./bin/gaiad-v7.0.3-darwin-amd64 q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
+    echo "First cmd:"
+    echo "\"./bin/gaiad-v7.0.3-darwin-amd64 q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
+        echo ""
     sleep 1
-    echo "Test it and repeat \"./bin/gaiad-v7.0.3-darwin-amd64 tx staking delegate cosmosvaloper1gddnzwdchpp24446k55tdhctfmjsg6ls947g7p 10000000stake --from tl --home app-home/ --keyring-backend test --chain-id autostake\""
+    echo "Second cmd:"
+    echo "\"./bin/gaiad-v7.0.3-darwin-amd64 tx staking delegate cosmosvaloper1gddnzwdchpp24446k55tdhctfmjsg6ls947g7p 10000000stake --from tl --home app-home/ --keyring-backend test --chain-id autostake\""
+    echo ""
     sleep 1
+    echo "Repeat this cmd to see changes:"
+    echo "\"./bin/gaiad-v7.0.3-darwin-amd64 q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
     exit 1
     else
-    echo "Test it with \"gaiad q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
+    echo "Test it and type in your cli in this project dir:"
     sleep 1
-    echo "Test it with \"gaiad tx staking delegate cosmosvaloper1gddnzwdchpp24446k55tdhctfmjsg6ls947g7p 10000000stake --from tl --home app-home/ --keyring-backend test --chain-id autostake\""
+    echo "First cmd:"
+    echo "\"gaiad q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
+    echo ""
     sleep 1
-    echo "Test it and repeat \"gaiad q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
+    echo "Second cmd:"
+    echo "\"gaiad tx staking delegate cosmosvaloper1gddnzwdchpp24446k55tdhctfmjsg6ls947g7p 10000000stake --from tl --home app-home/ --keyring-backend test --chain-id autostake\""
+    echo ""
     sleep 1
+    echo "Repeat this cmd to see changes:"
+    echo "\"gaiad q bank balances cosmos1hk06zl6krdnamxljwheq7372kf5g6688gmdhc3\""
     exit 1
 fi
-
-
